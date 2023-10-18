9e42c4642a9af1a7db5c4d99d677f83ca89129ab
Constantine_altan <constantine.altan@gmail.com>
2023-10-18 17:31:56 +0300

    added bootstrap

---
+++ public/api/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+define('PDO_DSN', "mysql:host=localhost;dbname=tree_test");
+define('PDO_USERNAME', "tree_test");
+define('PDO_PASSWORD', "super_tree_pass");
--- public/api/index.php
+++ public/api/index.php
@@ -1,23 +1,20 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+use Altan\TreeBuilder\Controller\TreeController;
+use Altan\TreeBuilder\Tools\Db\MysqlDb;
+use Altan\TreeBuilder\View\JsonView;
 
+require_once "bootstrap.php";
 require_once "../../vendor/autoload.php";
 
 $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $uri  = explode("/", trim($uri, "/"));
 while(array_shift($uri) != 'api');
 
-$db = new \Altan\TreeBuilder\Tools\Db\MysqlDb(
-	"mysql:host=localhost;dbname=tree_test",
-	"tree_test",
-	"super_tree_pass"
-);
+$db = new MysqlDb(PDO_DSN, PDO_USERNAME, PDO_PASSWORD);
 
 if($uri[0] == "tree") {
-    $controller = new \Altan\TreeBuilder\Controller\TreeController($db);
+    $controller = new TreeController($db);
     $controller->execute($uri, $_SERVER["REQUEST_METHOD"]);
-    $view = new \Altan\TreeBuilder\View\JsonView($controller->getStatus(), $controller->getData());
+    $view = new JsonView($controller->getStatus(), $controller->getData());
     $view->write();
 }
